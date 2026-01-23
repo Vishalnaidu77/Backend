de7fc3887e41c8d4f1020d77b082301f3c2674db
Vishalnaidu77 <vminorr@gmail.com>
2026-01-23 09:54:42 +0530

    REST API & Postman

--- day-2/index.js
+++ day-2/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express() // Server ka instance create karna
 
 app.get('/', (req, res) => {
-    res.send("Jai Shree Ram")
+    res.send("Hello World")
 })
 
 app.get('/about', function(req, res){
@@ -10,7 +10,7 @@ app.get('/about', function(req, res){
 })
 
 app.get('/product', (req, res) => {
-    res.send("Radhe Radhe")
+    res.send("Product page")
 })
 
-app.listen(3000) // Server start karna 
+app.listen(3000) // Server start karna S
