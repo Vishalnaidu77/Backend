1b1d4fbba64846f56c0f804bd56f61c43549cb91
Vishal Naidu <vminorr@gmail.com>
2026-02-19 23:02:50 +0530

    feat: enhance note management with improved form handling and UI updates

--- day-8/Frontend/src/App.jsx
+++ day-8/Frontend/src/App.jsx
@@ -5,60 +5,121 @@ import { useEffect } from 'react'
 const App = () => {
 
   const [notes, setNotes] = useState([])
+  const [isUpdate, setIsUpdate] = useState(false)
+  const [editingNoteId, seteditingNoteId] = useState('')
+  const [updatedDesc, setUpdatedDesc] = useState('')
+  const [titleInput, setTitleInput] = useState('')
+  const [descriptionInput, setDescriptionInput] = useState('')
 
-  useEffect(() => {
-    getData()
-  }, [])
-
-  const getData = async () => {
+  const fetchData = async () => {
     const { data } = await axios.get("http://localhost:3000/notes")
     setNotes(data.notes)
-  } 
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { title, description, type } = e.target.elements
+    e.preventDefault()
+    const { title, description, type } = e.target.elements;
+
 
     await axios.post("http://localhost:3000/notes", {
-      title: title.value,
-      description: description.value,
+      title: titleInput,
+      description: descriptionInput,
       type: type.value
     })
-    getData()
-  }
 
-  const handleDeleteNote = async (noteId) => {
+    setTitleInput('')
+    setDescriptionInput('')
+
+    fetchData()
+  } 
+
+  const handleDelete = async (noteId) => {
     await axios.delete(`http://localhost:3000/notes/${noteId}`)
-    getData()
+    fetchData()
   }
 
-  const handleUpdateNote = async (noteId) => {
-    await axios.patch(`http://localhost:3000/notes/${noteId}`, )
+  const handleUpdate = async (noteId) => {
+    await axios.patch(`http://localhost:3000/notes/${noteId}`, {
+      description: updatedDesc
+    })
+    setIsUpdate(false)
+    seteditingNoteId('')
+    setUpdatedDesc('')
+    fetchData()
+  }
+
+  const handleEditClick = (note) => {
+    if (isUpdate && editingNoteId === note._id) {
+      handleUpdate(note._id)
+      return
+    }
+
+    seteditingNoteId(note._id)
+    setUpdatedDesc(note.description)
+    setIsUpdate(true)
   }
 
   return (
     <div>
-
       <form className='note-create-form' onSubmit={handleSubmit}>
-        <input name='title' type="text" placeholder='Enter title'/>
-        <input name='description' type="text" placeholder='Enter description'/>
+        <input 
+          name='title' 
+          value={titleInput} 
+          onChange={(e) => setTitleInput(e.target.value)} 
+          type="text" 
+          placeholder='Enter Title'
+        />
+        <input 
+          name='description' 
+          type="text" 
+          placeholder='Enter Description'
+          value={descriptionInput}
+          onChange={(e) => setDescriptionInput(e.target.value)}
+          />
         <select name="type" id="">
           <option value="Note">Note</option>
           <option value="Task">Task</option>
-          <option value="Document">Document</option>
           <option value="Resource">Resource</option>
+          <option value="Document">Document</option>
         </select>
-        <button>Create Note</button>
+        <button type='submit'>Submit</button>
       </form>
 
       <div className="notes">
         {notes.map((note, idx) => (
         <div key={idx} className="note">
-          <h1>{note.title}</h1>
-          <p>{note.description}</p>
-          <p>{note.type}</p>
-          <button onClick={() => handleUpdateNote(note._id)}>Edit description</button>
-          <button onClick={() => handleDeleteNote(note._id)}>delete</button>
+          <div className="heading">
+            <h1>{note.title}</h1>
+            <p className='type'>{note.type}</p>
+          </div>
+          {note._id === editingNoteId && isUpdate
+            ? (
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault()
+                  handleUpdate(note._id)
+                }}
+              >
+                <input name='updatedDescription' type="text" value={updatedDesc} onChange={(e) => setUpdatedDesc(e.target.value)} />
+              </form>
+            ) 
+            : (
+              <p>{note.description}</p>
+            )
+          }
+          <div className="notes-btn">
+            <button 
+              type='button'
+              onClick={() => handleEditClick(note)}
+              >
+                {isUpdate && editingNoteId === note._id ? 'Submit' : 'Edit'}
+            </button>
+            <button onClick={() => handleDelete(note._id)}>delete</button>
+          </div>
         </div>
           ))
         }
