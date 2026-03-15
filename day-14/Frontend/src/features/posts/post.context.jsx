9748746b7ef08fbd5f3142971ddcc83a27e6f0ec
Vishal Naidu <vminorr@gmail.com>
2026-03-15 12:36:08 +0530

    feat: add react-icons for enhanced post interaction and update styles for better UI

--- day-14/Frontend/src/features/posts/post.context.jsx
+++ day-14/Frontend/src/features/posts/post.context.jsx
@@ -6,7 +6,7 @@ const PostProvider = ({ children }) => {
 
     const [ loading, setLoading ] = useState(false)
     const [post, setPost] = useState(null)
-    const [feed , setFeed] = useState(null )
+    const [feed , setFeed] = useState(null)
 
   return (
     <PostContext.Provider value={{ loading, setLoading, post, setPost, feed, setFeed }}> 
