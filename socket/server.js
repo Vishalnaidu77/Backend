7e17150fe6c1b069a24b9e0e5fa990c199af38fc
vishalnaidu77 <vminorr@gmail.com>
2026-05-05 16:50:10 +0530

    Implement socket.io project.

---
+++ socket/server.js
@@ -0,0 +1,22 @@
+import app from './src/app.js'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+
+})
+
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+
+    socket.on("message", (msg) => {
+        console.log(msg);
+        io.emit("demo", msg)
+    })
+})
+
+const PORT = 8000
+
+httpServer.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
